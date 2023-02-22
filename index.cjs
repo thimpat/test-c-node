dab46fd9f22ed6596777bfa132c30479855f8d98
Patrice THIMOTHEE <thimpat@gmail.com>
2023-02-22 00:45:23 +0000

    DEMO: Update demo

--- index.cjs
+++ index.cjs
@@ -1,15 +1,16 @@
 const {mkdirSync} = require("fs");
 const {rimrafSync} = require("rimraf");
-const {runFile, runLive, compileSource, compileLibrary, runBinary, invokeFunction, loadFunctions} = require("@thimpat/c-node");
+const {joinPath} = require("@thimpat/libutils");
+const {runFile, runLive, compileSource, compileLibrary, runBinary, invokeFunction, loadFunctions, loadBinaryFunctions} = require("@thimpat/c-node");
 
 const showHello = function ()
 {
     // Compile then run generated executable
-    runFile("examples/ex1.c", {outputDir: "demo/"});
+    runFile("examples/ex1.c", {outputDir: "demo-1/"});
 
-    runFile("examples/hello_win.c", {outputDir: "demo/"});
+    runFile("examples/hello_win.c", {outputDir: "demo-1/"});
 
-    const compiledCode = compileSource("examples/hello_win.c", {outputDir: "demo/"});
+    const compiledCode = compileSource("examples/hello_win.c", {outputDir: "demo-1/"});
     if (!compiledCode)
     {
         return ;
@@ -19,31 +20,51 @@ const showHello = function ()
     runLive("examples/hello_win.c");
 
     // Run generated executable
-    runBinary("demo/hello_win.exe", {outputDir: "demo/"});
+    runBinary("demo-1/hello_win.exe", {outputDir: "demo-1/"});
 }
 
 const generateDll = function ()
 {
-    // Compile then run generated executable
-    if (compileLibrary("examples/dll.c", {outputDir: "demo/"}))
+    process.chdir(__dirname);
+    const {success, compiledPath} = compileLibrary("examples/dll.c", {outputDir: "demo-1/"});
+    // run generated executable
+    if (!success)
     {
-        runFile("examples/hello_dll.c", {outputDir: "demo/", defs: ["demo/dll.dll"]});
+        console.error(`Compilation error`);
+        return ;
     }
+
+    runFile("examples/hello_dll.c", {outputDir: "demo-1/", defs: [compiledPath]});
+
 }
 
 const callFunction = function ()
 {
-    const result = invokeFunction("hello_func()", "examples/dll.c", {outputDir: "demo/"});
+    const result = invokeFunction("hello_func()", "examples/dll.c", {cwd: "demo-2"});
     console.log({lid: 3002}, result);
+}
+
+const loadSourceLibrary = function ()
+{
+    process.chdir(joinPath(__dirname, "./demo-1"));
+    const {hello_func} = loadFunctions("../examples/dll.c", {
+        hello_func: {
+            prototype: "char* hello_func (char*)",
+        }
+    }, {outputDir: "./"});
+
+    const res = hello_func("My name is awesome");
+    console.log({lid: "NC6452"}, res);
 }
 
 const loadDll = function ()
 {
-    const {hello_func} = loadFunctions("examples/dll.c", {
+    process.chdir(joinPath(__dirname, "./resources"));
+    const {hello_func} = loadBinaryFunctions("dll.dll", {
         hello_func: {
             prototype: "char* hello_func (char*)",
         }
-    }, {outputDir: "demo/"});
+    });
 
     const res = hello_func("My name is awesome");
     console.log({lid: "NC6452"}, res);
@@ -51,12 +72,13 @@ const loadDll = function ()
 
 const init = async function (argv)
 {
-    rimrafSync("demo/");
-    mkdirSync("demo", {recursive: true});
+    rimrafSync("demo-1/");
+    mkdirSync("demo-1", {recursive: true});
 
-    // showHello();
-    // generateDll();
-    // callFunction();
+    showHello();
+    generateDll();
+    callFunction();
+    loadSourceLibrary();
     loadDll();
 };
 
